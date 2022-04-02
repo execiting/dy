2bede06f0171500479b5726cb19a1f18bff492d5
execiting <101497929+execiting@users.noreply.github.com>
2022-04-03 00:56:00 +0800

    Add files via upload

---
+++ admin/foot.php
@@ -0,0 +1,1 @@
+<div id="footer">Copyright &copy; <span style="color:#FF5200">www.x6d.com</span><span style="color:#0065FF">.Com</span> , Powered by <a href="https://www.x6d.com" target="_blank">源码下载</a></div>
---
+++ inc/aik.config.php
@@ -0,0 +1,65 @@
+<?php
+ $aik =  array (
+  'sitename' => '小刀娱乐网',
+  'pcdomain' => 'https://www.x6d.com',
+  'title' => '﻿爱客影院-免VIP抢先观看最新好看的电影和电视剧',
+  'keywords' => '爱客影院,电视直播网站,零八影院快播,高清云影视,云点播,免费看视频,湖南卫视直播,80电影网,最新电影天堂免费在线观看',
+  'description' => '﻿爱客影院,热剧快播,最好看的剧情片尽在﻿爱客影院,高清云影视免费为大家提供最新最全的免费电影，电视剧，综艺，动漫无广告在线云点播，以及电视直播',
+  'icp' => '国ICP备18888888号',
+  'foot' => '本站提供的最新电影和电视剧资源均系收集于各大视频网站,本站只提供web页面服务,并不提供影片资源存储,也不参与录制、上传</br>若本站收录的节目无意侵犯了贵司版权，请给网页底部邮箱地址来信,我们会及时处理和回复,谢谢。',
+  'tongji' => '<script language="javascript" type="text/javascript" src="http://js.users.51.la/18759442.js"></script>',
+  'youlian' => '<a target="blank" class="gobtn" href="http://kuzhan.la">高防服务器</a>
+<a target="blank" class="gobtn" href="http://www.xiaodao.biz">小刀娱乐网</a>',
+  'shouquan' => 'NzNiYmQwNGI2YjUxZWViODIzNDI2YzgwODBkMWI0YTQ2NDJmOQ==',
+  'zhanwai' => 'http://www.123ku.com/inc/feifei3.4/',
+  'jiekou2' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou3' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou4' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou5' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou6' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou7' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou8' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou9' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou10' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'jiekou1' => 'http://h.zy40.cn/yjx/index.php?url=',
+  'admin_name' => 'admin',
+  'admin_pass' => '3ceb0e9fb16f8673c35f707e8657124a',
+  'admin_email' => 'admin@admin.com',
+  'logo_dh' => '<img src="images/logo.png">',
+  'logo_ss' => '<img id="imgsrc" src="images/sologo.png">',
+  'movie_ad' => '<a href="http://www.x6d.com" target="_blank"><img src="https://www.x6d.com/static/images/logo.png" style="width:100%"></a>',
+  'tv_ad' => '<a href="http://www.x6d.com" target="_blank"><img src="https://www.x6d.com/static/images/logo.png" style="width:100%"></a>',
+  'zongyi_ad' => '<a href="http://www.x6d.com" target="_blank"><img src="https://www.x6d.com/static/images/logo.png" style="width:100%"></a>',
+  'dongman_ad' => '<a href="http://www.x6d.com" target="_blank"><img src="https://www.x6d.com/static/images/logo.png" style="width:100%"></a>',
+  'bofang_ad' => '<a href="http://www.x6d.com" target="_blank"><img src="https://www.x6d.com/static/images/logo.png" style="width:100%"></a>',
+  'jiazai_ad' => '<a href="http://www.x6d.com" target="_blank"><img src="https://www.x6d.com/static/images/logo.png" style="width:100%"></a>',
+  'tishi_ad' => '<style> 
+ .black_overlay{  display: none;  position: absolute;  top: 0%;  left: 0%;  width: 100%;  height: 100%;  background-color: black;  z-index:1001;  -moz-opacity: 0.8;  opacity:.80;  filter: alpha(opacity=80);  }  .white_content {  display: none;  position: absolute;  top: 25%;  left: 25%;  width: 50%;   height: auto; padding: 16px;  border: 16px solid orange;  background-color: white;  z-index:1002;  overflow: auto;  }  </style> 
+<a href="javascript:void(0)" onclick="document.getElementById(\'light\').style.display=\'block\';document.getElementById(\'fade\').style.display=\'block\'"><p style="text-align:center;color: #fff;font-size: 10px;background: #6ED56C;padding:11px 8px;border-radius: 2px;">点击关注“智云影院”官方发布页，看电影更方便!</p></a>
+<div id="light" class="white_content"> 
+  <img src="images/qrcode.png" width="100%" height="100%">
+    <a href="javascript:void(0)" onclick="document.getElementById(\'light\').style.display=\'none\';document.getElementById(\'fade\').style.display=\'none\'"> 
+    <span class="rewards-popover-close" etap="rewards-close"></span></a></div> 
+<div id="fade" class="black_overlay"> 
+</div> ',
+  'dbts' => '【点击选择剧集才会播放哦！】',
+  'zfb_ad' => '<img src="images/zfb.png">',
+  'wx_ad' => '<img src="images/wx.png">',
+  'cebian1_ad' => '',
+  'cebian2_ad' => '',
+  'cebian3_ad' => '',
+  'cebian4_ad' => '',
+  'top1_ad' => '<a href="app.html">客户端</a>',
+  'top2_ad' => '',
+  'top_ad' => '',
+  'weixin_ad' => '<img src="images/qrcode.png">',
+  'end_ad' => '<a href="dongmanh.php"><span><img src="images/gouwu.png"/>动漫</span></a>',
+  'dtk_ad' => 'http://www.2xb.cn/',
+  'qq_name' => '醉玲珑#无证之罪#权力的游戏第七季',
+  'hometopnotice' => '',
+  'hometopright' => '',
+  'homelink' => '',
+  'sort' => '1,2,3,4,5,6,7,8,9,10',
+  'joinhotkey' => '0',
+);
+?>
